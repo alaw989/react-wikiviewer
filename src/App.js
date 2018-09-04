9ae613ce0e0e78dd2ded7f6c7ec1ff187a023204
Austin Law <alaw989@gmail.com>
2018-09-03 21:40:50 -0400

    separated api component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {asyncContainer, Typeahead} from 'react-bootstrap-typeahead';
-
-const AsyncTypeahead = asyncContainer(Typeahead);
-
-const urlForWiki = query => `https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&apfrom=${query}&aplimit=3`
+import Search from './Search.js';
 
 class App extends Component {
   constructor(props) {
@@ -17,30 +13,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-              <AsyncTypeahead
-                onSearch={query =>
-                  fetch(urlForWiki(query))
-                    .then(data => data.json())
-                    .then(data => {
-                      console.log(data.query.allpages)
+              <Search
+                // onSearch={query =>
+                //   fetch(urlForWiki(query))
+                //     .then(data => data.json())
+                //     .then(data => {
+                //       console.log(data.query.allpages)
                     
-                      const titles = [];
-                      // query.map(x => titles.push(x.title));
-                      this.setState({
-                        options: titles
-                      });
-                    })}
-                options={this.state.options}
-                onKeyDown={this.handleAction}
-                onChange={this.handleClick}
-                onActiveItemChange={this.active}
-                placeholder="Search Movie Title..."
-                maxResults={5}
-                emptyLabel="No Matches Found"
-                searchText=""
-                propText=""
-                paginationText=""
-                delay={300}
+                //       const titles = [];
+                //       // query.map(x => titles.push(x.title));
+                //       this.setState({    
+                //         options: titles
+                //       });
+                //     })}
+                // options={this.state.options}
+                // onKeyDown={this.handleAction}
+                // onChange={this.handleClick}
+                // onActiveItemChange={this.active}
+                // placeholder="Search Movie Title..."
+                // maxResults={5}
+                // emptyLabel="No Matches Found"
+                // searchText=""
+                // propText=""
+                // paginationText=""
+                // delay={300}
               />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
---
+++ src/Search.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { asyncContainer, Typeahead } from 'react-bootstrap-typeahead';
+
+const AsyncTypeahead = asyncContainer(Typeahead);
+
+const urlForWiki = query => `https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&apfrom=${query}&aplimit=3`
+
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <AsyncTypeahead
+                onSearch={query =>
+                    fetch(urlForWiki(query))
+                        .then(data => data.json())
+                        .then(data => {
+                            const results = data.query.allpages
+                           
+                            const titles = results.map(x => {
+                               
+                                return x.titles;
+                            });
+
+
+                            console.log(titles)
+                            // query.map(x => titles.push(x.title));
+                            this.setState({
+                                options: titles
+                            });
+                        })}
+                options={this.state.options}
+                onKeyDown={this.handleAction}
+                onChange={this.handleClick}
+                onActiveItemChange={this.active}
+                placeholder="Search Movie Title..."
+                maxResults={5}
+                emptyLabel="No Matches Found"
+                searchText=""
+                propText=""
+                paginationText=""
+                delay={300}
+            />
+        );
+    }
+}
+
+export default Search;
