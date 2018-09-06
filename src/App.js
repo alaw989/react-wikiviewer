6a6710691f94012f443f9928c5136c35999fbfe0
Austin Law <alaw@SMG.local>
2018-09-06 17:47:01 -0400

    styled typeahead dropdown

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Search from './Search.js';
+import React, { Component } from "react";
+import "./App.css";
+import { asyncContainer, Typeahead } from "react-bootstrap-typeahead";
+import styled from "styled-components";
+
+const AsyncTypeahead = asyncContainer(Typeahead);
+
+const urlForWiki = query =>
+  `https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&apfrom=${query}&aplimit=7`;
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      options: []
+    };
+    this.handleKeyPress = this.handleEnter.bind(this);
   }
 
+  handleEnter = (e) => {
+
+    console.log(e.target.value)
+    if (e.key === "Enter") {
+
+    }
+
+  };
 
   render() {
     return (
       <div className="App">
-              <Search />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Wikipedia Viewer</h1>
+          <AsyncTypeahead
+            onSearch={query =>
+              fetch(urlForWiki(query))
+                .then(data => data.json())
+                .then(data => {
+                  const results = data.query.allpages;
+                  const titles = [];
+                  console.log(data)
+                  results.map(x => {
+                    titles.push(x.title);
+                  });
+
+              
+
+                  this.setState({
+                    options: titles
+                  });
+                })
+            }
+            options={this.state.options}
+            onKeyDown={this.handleEnter}
+            // onChange={this.handleClick}
+            // onActiveItemChange={this.active}
+            placeholder="Search Wikipedia..."
+            isLoading={false}
+            maxResults={6}
+            emptyLabel="No Matches Found"
+            searchText=""
+            propText=""
+            paginationText=""
+            delay={300}
+          />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
       </div>
     );
   }
 }
 
 export default App;
-
-
--- src/Search.js
+++ src/Search.js
@@ -1,54 +1,62 @@
-import React, { Component } from 'react';
-import { asyncContainer, Typeahead } from 'react-bootstrap-typeahead';
+import React, { Component } from "react";
+import { asyncContainer, Typeahead } from "react-bootstrap-typeahead";
 
 const AsyncTypeahead = asyncContainer(Typeahead);
 
-const urlForWiki = query => `https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&apfrom=${query}&aplimit=3`
-
+const urlForWiki = query =>
+  `https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&apfrom=${query}&aplimit=5`;
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            options: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: []
+    };
+    this.handleKeyPress = this.handleEnter.bind(this);
+  }
+
+  handleEnter = () => {
+      
+  }
+
+
+  render() {
+    return (
+      <AsyncTypeahead
+        onSearch={query =>
+          fetch(urlForWiki(query))
+            .then(data => data.json())
+            .then(data => {
+
+              const results = data.query.allpages;
+              const titles = [];
+
+              results.map(x => {
+                titles.push(x.title);
+              });
+
+              console.log(titles);
+
+              this.setState({
+                options: titles
+              });
+            })
         }
-    }
-    render() {
-        return (
-            <AsyncTypeahead
-                onSearch={query =>
-                    fetch(urlForWiki(query))
-                        .then(data => data.json())
-                        .then(data => {
-                            const results = data.query.allpages
-                           const titles = [];
-                         results.map(x => {
-                               console.log(x)
-                                titles.push(x.title)
-
-                            });
-
-
-                            console.log(titles)
-                            // query.map(x => titles.push(x.title));
-                            this.setState({
-                                options: titles
-                            });
-                        })}
-                // options={this.state.options}
-                // onKeyDown={this.handleAction}
-                // onChange={this.handleClick}
-                // onActiveItemChange={this.active}
-                placeholder="Search Movie Title..."
-                maxResults={5}
-                emptyLabel="No Matches Found"
-                searchText=""
-                propText=""
-                paginationText=""
-                delay={300}
-            />
-        );
-    }
+        options={this.state.options}
+        onKeyDown={this.handleEnter}
+        // onChange={this.handleClick}
+        // onActiveItemChange={this.active}
+        placeholder="Search Wikipedia..."
+        isLoading= {false}
+        maxResults={5}
+        emptyLabel="No Matches Found"
+        searchText=""
+        propText=""
+        paginationText=""
+        delay={300}
+      />
+    );
+  }
 }
 
 export default Search;
