23ead521d8ed9c15db4da0b9fdcd8553494f2009
Austin Law <alaw989@gmail.com>
2018-09-09 22:31:13 -0400

    started building event functions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import styled from "styled-components";
 const AsyncTypeahead = asyncContainer(Typeahead);
 
 const urlForWiki = query =>
-  `https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&apfrom=${query}&aplimit=7`;
+  `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&titles=${query}&srsearch=${query}`;
+
+
 
 class App extends Component {
   constructor(props) {
@@ -14,16 +16,33 @@ class App extends Component {
     this.state = {
       options: []
     };
-    this.handleKeyPress = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleAction.bind(this);
   }
 
-  handleEnter = (e) => {
+  handleAction = (e) => {
 
-    console.log(e.target.value)
     if (e.key === "Enter") {
 
     }
 
+    let value = e.toString("");
+    if (e.length > 0) {
+      fetch(urlForWiki(value))
+        .then(data => data.json())
+        .then(data => {
+          console.log(data.query.search)
+          const wikiResults = data.query.search
+          // let movie = data.results;
+          // const titles = [];
+          // movie.map(x => (x.title === value ? titles.push(x) : ""));
+          // if (titles.length < 1) {
+          //   return null;
+          // }
+          this.setState({
+            options: wikiResults
+          });
+        });
+    }
   };
 
   render() {
@@ -35,14 +54,21 @@ class App extends Component {
               fetch(urlForWiki(query))
                 .then(data => data.json())
                 .then(data => {
-                  const results = data.query.allpages;
-                  const titles = [];
-                  console.log(data)
-                  results.map(x => {
-                    titles.push(x.title);
+                  const results = data.query.search;
+                  const titles = results.map(x => {
+                    return x.title
                   });
 
-              
+                  // const concatArray = titles.reduce(function (result, value, index, array) {
+                  //   if (index % 2 === 0)
+                  //     result.push(array.slice(index, index + 2));
+                  //   return result;
+                  // }, []);
+
+                  // const arrayToString = concatArray.map(array => {
+                  //   array = array.toString('').replace(/<span class="searchmatch">/g, '').replace(/\//g, '').replace(/<span>/g, '').replace(/&quot;/g, '"').replace(',', ' - ');
+                  //   return array;
+                  // });
 
                   this.setState({
                     options: titles
@@ -50,12 +76,12 @@ class App extends Component {
                 })
             }
             options={this.state.options}
-            onKeyDown={this.handleEnter}
-            // onChange={this.handleClick}
-            // onActiveItemChange={this.active}
+            onKeyDown={this.handleAction}
+            onChange={this.handleAction}
+            onActiveItemChange={this.active}
             placeholder="Search Wikipedia..."
             isLoading={false}
-            maxResults={6}
+            maxResults={10}
             emptyLabel="No Matches Found"
             searchText=""
             propText=""
